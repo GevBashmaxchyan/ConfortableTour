b8a5b55fe0aa2fbdf0c62243b5f3954d71315231
Gevorg Bashmaxchyan <gev.bashmaxchyan@mail.ru>
2022-03-20 15:03:37 +0400

    Initial commit

---
+++ src/main/java/com/example/confortabletour/ConfortableTourApplication.java
@@ -0,0 +1,13 @@
+package com.example.confortabletour;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConfortableTourApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConfortableTourApplication.class, args);
+    }
+
+}
